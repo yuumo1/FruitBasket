0b3b4153883ef34651a715011824fab4de77660a
Erika Bautista <ekabells19@gmail.com>
2024-01-16 21:20:42 +0800

    Update FruitBasket.java

--- src/main/java/com/mycompany/fruitbasket/FruitBasket.java
+++ src/main/java/com/mycompany/fruitbasket/FruitBasket.java
@@ -8,9 +8,85 @@ package com.mycompany.fruitbasket;
  *
  * @author Erika
  */
+import java.util.Scanner;
+import java.util.Stack;
+
 public class FruitBasket {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public static void main(String[] args) {
+
+Scanner scanner = new Scanner(System.in);
+
+Stack<String> basket = new Stack<String>();
+
+System.out.print("Enter no. Of fruits you want to eat. 1 - 4 FRUITS ");
+
+int numFruits = scanner.nextInt();
+
+for (int i = 1; i <= numFruits; i++) {
+
+System.out.print("Choose a fruit to catch. Press A, O, M, or G. ");
+
+String fruit = scanner.next().toUpperCase();
+
+if (fruit.equals("A")) {
+
+basket.push("apple");
+
+} else if (fruit.equals("O")) {
+
+basket.push("orange");
+
+} else if (fruit.equals("M")) {
+
+basket.push("mango");
+
+} else if (fruit.equals("G")) {
+
+basket.push("guava");
+
+} else {
+
+System.out.println("Invalid fruit choice. Try again. ");
+
+i--;
+
+}
+
+}
+
+System.out.println("Your basket now has: " + basket.toString());
+
+while (!basket.isEmpty()) {
+
+System.out.print("Press E to eat a fruit. ");
+
+String eat = scanner.next().toUpperCase();
+
+if (eat.equals("E")) {
+
+basket.pop();
+
+if (!basket.isEmpty()) {
+
+System.out.println("Fruit(s) in the basket: " + basket.toString());
+
+} else {
+
+System.out.println("You already eat your fruits. ");
+
+}
+
+} else {
+
+System.out.println("Invalid choice. Try again. ");
+
+}
+
+}
+
+System.out.println("Basket is empty. ");
+
+}
+
 }
